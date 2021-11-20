03107755f7fa7329efe58a929fe92b14774cd860
Mega Poverty <ethan@Ethans-MacBook-Pro-2.local>
2021-11-19 17:10:57 -0800

    Enable Eth button, Account is shown

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-            <h1>Hello world, this is a test app.</h1><br/>
-          <p>
-            The goal is to connect to your eth wallet and tell you how much ohm you make per day.
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>Hello world, this is a test app.</h1><br/>
+                <p>
+                    The goal is to connect to your eth wallet and tell you how much ohm you make per day.
+                </p>
+            </header>
+            <body>
+            </body>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/EnableButton.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+export class EnableButton extends Component {
+    render() {
+        return (
+            <div>
+                <button className="enableEthereumButton" onClick={connectMask}>Enable Ethereum</button>
+            </div>
+        );
+    }
+}
+
+async function connectMask() {
+    let ethereum;
+    const showAccount = document.querySelector('.showAccount');
+    ethereum = window.ethereum;
+    const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+    const account = accounts[0];
+    // We currently only ever provide a single account,
+    // but the array gives us some room to grow.
+    showAccount.innerHTML = account;
+}
